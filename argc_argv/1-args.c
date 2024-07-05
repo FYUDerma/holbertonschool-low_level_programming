8594df2c5786adcda85f258f3896a06a1f36fce4
FYUDerma <zakaria.12ans@gmail.com>
2024-07-05 09:39:45 +0200

    Write a program that prints the number of arguments passed into it.
    Your program should print a number, followed by a new line

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc : Argc (count)
+ * @argv : Argv (vector)
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	UNUSED(argv);
+	printf("%d\n", argc);
+	return (0);
+}
