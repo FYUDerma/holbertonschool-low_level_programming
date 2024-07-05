7324b34bb208a371dc4fa5a3bcc18d9634af1604
FYUDerma <zakaria.12ans@gmail.com>
2024-07-05 09:32:49 +0200

    Write a program that prints its name, followed by a new line.

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc : Argc (count)
+ * @argv : Argv (vector)
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i; /* Index */
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
