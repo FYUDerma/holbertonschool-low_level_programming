9f8ef999c4119c09c9b8f226f313a11d2efbf6bc
FYUDerma <zakaria.12ans@gmail.com>
2024-07-10 09:43:15 +0200

    moving and creating Project 2

---
+++ more_malloc_free/malloc_free/Model.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * reset_to_98 - Point n to 98
+ * @n : Pointer n
+ *
+ * Return: Always 0 (Success)
+ */
+
+
---
+++ more_malloc_free/malloc_free/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Stardart Libraries*/
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/* Standard Function */
+int _putchar(char c);
+
+/* Project Function Malloc */
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+
+#endif
