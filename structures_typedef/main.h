32fb4ed789a5149de17fba9ace4629b8e0d790d8
FYUDerma <zakaria.12ans@gmail.com>
2024-07-11 13:47:53 +0200

    C - Structures, typedef

---
+++ structures_typedef/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Stardart Libraries*/
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/* Standard Function */
+int _putchar(char c);
+
+#endif
