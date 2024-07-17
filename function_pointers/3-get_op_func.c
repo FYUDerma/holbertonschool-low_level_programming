2beb73fa445d278086e68a7ddc65f9571999c48b
FYUDerma <zakaria.12ans@gmail.com>
2024-07-17 10:12:43 +0200

    Write a program that performs simple operations.
    You are allowed to use the standard library
    Usage: calc num1 operator num2
    You can assume num1 and num2 are integers, so use the atoi function to convert them from the string input to int
    operator is one of the following:
    +: addition
    -: subtraction
    *: multiplication
    /: division
    %: modulo
    The program prints the result of the operation, followed by a new line
    You can assume that the result of all operations can be stored in an int
    if the number of arguments is wrong, print Error, followed by a new line, and exit with the status 98
    if the operator is none of the above, print Error, followed by a new line, and exit with the status 99
    if the user tries to divide (/ or %) by 0, print Error, followed by a new line, and exit with the status 100

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "3-calc.h"
+
+/**
+ * get_op_func - Finds the right operation
+ * @s: String character representing the operation
+ * Return: A pointer to a function
+ */
+int (*get_op_func(char *s))(int a, int b)
+{
+	int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
