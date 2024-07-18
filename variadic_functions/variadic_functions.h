e348d873f760dbe78bf886f3159536001246e238
FYUDerma <zakaria.12ans@gmail.com>
2024-07-18 20:07:41 +0200

    Modifying the main.h into Variagic_functions.h

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/* Stardart Libraries*/
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/* Function Prototype */
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
