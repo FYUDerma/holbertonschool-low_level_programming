5103de2c300ac133533f4b916c4e9f1f2f8e0e76
FYUDerma <zakaria.12ans@gmail.com>
2024-07-30 14:24:24 +0200

    Write a function that prints You're beat! and yet, you must allow,\nI bore my house upon my back!\n before the main function is executed.
    You are allowed to use the printf function

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * noFunC - Function that runs before Main
+ */
+
+void __attribute__((constructor)) noFunC(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
